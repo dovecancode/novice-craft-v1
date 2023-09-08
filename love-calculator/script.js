4d077fa23cf0c34f694fce8dfebbcdd81728a1da
dove <codegeekdove@gmail.com>
2023-09-08 14:29:43 +0800

    added the love calculator functionality

--- love-calculator/script.js
+++ love-calculator/script.js
@@ -1,20 +1,20 @@
 const pressBtnEl = document.getElementById('pressBtn') //button
-const resultEl = document.getElementById('result')
+const resultEl = document.querySelector('.result p')
 let guyName = document.getElementById('guyName')
 let girlName = document.getElementById('girlName')
 
-let love_score = Math.floor(Math.random() * 100) + 1
-
 function onClickCalculate() {
-  if (guyName.value.length > 0 && girlName.value.length > 0) {
+  let love_score = Math.floor(Math.random() * 100) + 1
+  resultEl.innerHTML = ''
+  if (guyName.value !== '' && girlName.value !== '') {
     if (love_score > 80) {
-      resultEl.innerHTML = '<p>Your love score is ' + love_score + '%.' + ' You are a good match!</p>'
+      resultEl.innerHTML = 'Your love score is ' + love_score + '%.' + ' You are a good match!.'
     } else if (love_score > 40 && love_score <= 80) {
-      resultEl.innerHTML = '<p>Your love score is ' + love_score + '%' + 'You are both sweet but    this is not the right time</p>'
+      resultEl.innerHTML = 'Your love score is ' + love_score + '%' + 'You are both sweet but    this is not the right time.'
     } else if (love_score <= 30) {
-      resultEl.innerHTML = '<p>Your love score is ' + love_score + '%' + ' You both have other priorities</p>'
+      resultEl.innerHTML = 'Your love score is ' + love_score + '%' + ' You both have other priorities.'
     } else {
-      resultEl.innerHTML = '<p>Your love score is ' + love_score + '% ' + ' I think you  are good to be friends not lovers.</p>'
+      resultEl.innerHTML = 'Your love score is ' + love_score + '% ' + ' I think you  are good to be friends not lovers.'
     }
   } else {
     alert('Dont leave input fields empty')
@@ -22,7 +22,6 @@ function onClickCalculate() {
 
   guyName.value = ''
   girlName.value = ''
-  // console.log(guyName)
 }
 
 pressBtnEl.addEventListener('click', onClickCalculate)
